a31f680ec6e8467709cc27dee0001d6edb1b12a3
Sven Schittecatte <sinodaurus@gmail.com>
2022-12-22 16:57:26 +0100

    It's working again :D

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use base64;
+use log::{debug, error, trace};
 use std::collections::{HashMap, VecDeque};
-use std::fmt::{self, Write};
-use std::net::{SocketAddr, ToSocketAddrs, UdpSocket};
+use std::fmt;
+use std::net::{SocketAddr, UdpSocket};
 use std::process::exit;
 use std::str::{self, FromStr};
 use std::time::Duration;
-use tokio;
 
-static SEND_SERVER_ADDR: &str = "127.0.0.1:8877";
-static RECV_SERVER_ADDR: &str = "127.0.0.1:8878";
+static RECV_SOCKET_PORT: u16 = 8877;
+static SEND_SOCKET_PORT: u16 = 8878;
+static SEND_SERVER_ADDR: [u8; 4] = [0, 0, 0, 0];
+static RECV_SERVER_ADDR: [u8; 4] = [0, 0, 0, 0];
+
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 struct ServerRuntime {
@@ -20,14 +23,16 @@ struct ServerRuntime {
 
 impl ServerRuntime {
     fn new() -> Self {
-        let send_socket = UdpSocket::bind(SEND_SERVER_ADDR).unwrap();
+        let send_socket =
+            UdpSocket::bind(SocketAddr::from((SEND_SERVER_ADDR, SEND_SOCKET_PORT))).unwrap();
         send_socket
             .set_read_timeout(Some(Duration::new(0, 1000)))
             .unwrap();
         send_socket
             .set_write_timeout(Some(Duration::new(0, 1000)))
             .unwrap();
-        let recv_socket = UdpSocket::bind(RECV_SERVER_ADDR).unwrap();
+        let recv_socket =
+            UdpSocket::bind(SocketAddr::from((RECV_SERVER_ADDR, RECV_SOCKET_PORT))).unwrap();
         recv_socket
             .set_read_timeout(Some(Duration::new(0, 1000)))
             .unwrap();
@@ -45,23 +50,26 @@ impl ServerRuntime {
     fn broadcast(&mut self, msg: &str) {
         for key in self.players.keys() {
             let decoded_src = base64::decode(key).unwrap();
-            match self.addrs.get(str::from_utf8(&decoded_src).unwrap()) {
-                Some(send_src) => {
-                    let socket_addr = SocketAddr::from_str(send_src).unwrap();
-                    println!("sending to {:?}", socket_addr);
-                    match self.send_socket.send_to(&msg.as_bytes(), socket_addr) {
-                        Ok(_) => {
-                            println!("sent {} to {:?}", msg, socket_addr);
-                            match self.send_socket.recv(&mut []) {
-                                Ok(_) => {
-                                    println!("recv from {:?}", socket_addr);
-                                }
-                                _ => {}
-                            }
-                        }
-                        _ => {}
+            let decoded_src = str::from_utf8(&decoded_src).unwrap();
+            let decoded_src_parts = decoded_src.split(":").collect::<Vec<_>>();
+            trace!("sending for key: {:?}", decoded_src);
+            trace!("sending for key parts: {:?}", decoded_src_parts);
+            let recv_port = u16::from_str(decoded_src_parts[1]).unwrap();
+            let send_port = recv_port + 1;
+            trace!("sending for key: {:?}", decoded_src);
+            trace!("sending to send port: {:?}", send_port);
+            match self.send_socket.send_to(
+                &msg.as_bytes(),
+                format!("{}:{}", decoded_src_parts[0], send_port),
+            ) {
+                Ok(_) => match self.send_socket.recv(&mut []) {
+                    Ok(_) => {
+                        trace!("recv from {:?}", decoded_src);
                     }
-                }
+                    Err(error) => {
+                        trace!("err recv from broadcast: {}", error)
+                    }
+                },
                 _ => {}
             }
         }
@@ -71,17 +79,15 @@ impl ServerRuntime {
         let mut buf = [0; 128];
         match self.recv_socket.recv_from(&mut buf) {
             Ok((number_of_bytes, src)) => {
-                // println!("number_of_bytes: {}", number_of_bytes);
-                // let src = SocketAddr::from_str("127.0.0.1:8767").unwrap();
+                let mut result_command: Option<String> = None;
                 if number_of_bytes > 1 {
                     let encoded_src = base64::encode(src.to_string());
                     let op_context = Self::get_context_from(&buf, number_of_bytes);
                     let player_data_vec: Vec<&str> = op_context.split(";").collect();
-                    println!("player_data in handle_message: {:?}", player_data_vec);
-                    // println!("operation: {}", Self::get_operation_from(&buf));
-                    return match Self::get_operation_from(&buf) {
+                    debug!("player_data in handle_message: {:?}", player_data_vec);
+                    debug!("operation: {}", Self::get_operation_from(&buf));
+                    result_command = match Self::get_operation_from(&buf) {
                         "S0;" => {
-                            println!("{:?}", player_data_vec);
                             let send_addr = player_data_vec[0];
                             if !self.addrs.contains_key(&src.to_string()) {
                                 self.addrs.insert(src.to_string(), send_addr.to_string());
@@ -105,15 +111,24 @@ impl ServerRuntime {
                         "E0;" => exit(0),
                         _ => Some("unknown".to_string()),
                     };
+                } else {
+                    debug!("no data");
                 }
                 match self.recv_socket.send_to(&[], src) {
-                    Ok(_) => {}
-                    _ => {}
+                    Ok(_) => {
+                        trace!("handle msg sent")
+                    }
+                    Err(error) => {
+                        trace!("ack handle msg: {}", error)
+                    }
                 }
+                return result_command;
             }
-            _ => {}
+            Err(error) => {
+                trace!("recv handle msg: {}", error);
+            }
         }
-        None
+        return None;
     }
 
     fn r#move(&mut self, player_id: String, direction: u8) -> String {
@@ -139,7 +154,7 @@ impl ServerRuntime {
 
     fn login(&mut self, player_id: String, player_data_vec: Vec<&str>) -> String {
         if self.players.contains_key(player_data_vec[0]) {
-            println!("Player already logged in...");
+            debug!("Player already logged in...");
             let player = self.players.get(player_data_vec[0]).unwrap();
             format!("{}", player)
         } else {
@@ -153,7 +168,7 @@ impl ServerRuntime {
                 0,
             );
             self.players.insert(player_id, player_data.clone());
-            println!("New user logs in.  Current players: {:?}", self.players);
+            debug!("New user logs in.  Current players: {:?}", self.players);
             format!("{}", &player_data)
         }
     }
@@ -215,7 +230,7 @@ impl Player {
         }
     }
     fn to_move_str(&self) -> String {
-        // "P0;blub_id;Primal;2;{};{};1;0;0",
+        // format: "P0;blub_id;Primal;2;{};{};1;0;0",
         let props: Vec<String> = vec![
             "P0".to_string(),
             self.id.to_string(),
@@ -243,40 +258,28 @@ impl fmt::Display for Player {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
+    env_logger::init();
     let mut runtime: ServerRuntime = ServerRuntime::new();
     let mut messages: VecDeque<String> = VecDeque::new();
     loop {
-        // runtime.send_socket.connect("127.0.0.1:9999")?;
-        // let test = runtime.send_socket.send(b"test")?;
-        // println!("sent test: {:?}", test);
-        // println!("what is on the stack: {:?}", messages);
-        // tokio::select! {
-        let next_msg = pop_front_message(&mut messages);
+        trace!("what is on the stack: {:?}", messages);
+        let next_msg = messages.pop_front();
         match next_msg {
             Some(pop_msg) => {
-                println!("pop_front: {}", pop_msg);
+                debug!("pop_front: {}", pop_msg);
                 runtime.broadcast(&pop_msg);
             }
             _ => {}
         }
-        let handled_msg = handle_message(&mut runtime);
+        let handled_msg = runtime.handle_message();
         match handled_msg {
             Some(msg) => {
-                println!("handled_msg to push back on stack: {}", msg);
+                trace!("handled_msg to push back on stack: {}", msg);
                 messages.push_back(msg);
             }
             _ => {}
         }
-        ::std::thread::sleep(Duration::new(0, 1_000));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
     }
-}
-
-fn handle_message(runtime: &mut ServerRuntime) -> Option<String> {
-    runtime.handle_message()
-}
-
-fn pop_front_message(messages: &mut VecDeque<String>) -> Option<String> {
-    messages.pop_front()
 }
